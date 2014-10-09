295a1a52f60bc9d981a6b77a5f777649613a4eb4
Tomoya Kose <tomoya@mitsuse.jp>
2014-10-10 00:58:42 +0900

    Write the interface of ProgressBar roughly.

--- progress.go
+++ progress.go
@@ -1,1 +1,44 @@
 package progress
+
+type ProgressBar interface {
+	Progress() (progress uint)
+	Task() (task uint)
+	Show()
+	Add(progress uint)
+}
+
+type progressBar struct {
+	progress uint
+	task     uint
+}
+
+func New(task uint) ProgressBar {
+	p := &progressBar{
+		progress: 0,
+		task:     task,
+	}
+
+	return p
+}
+
+func (p *progressBar) Progress() (progress uint) {
+	progress = p.progress
+	return
+}
+
+func (p *progressBar) Task() (task uint) {
+	task = p.task
+	return
+}
+
+func (p *progressBar) Show() {
+	// TODO: Implement this method.
+}
+
+func (p *progressBar) Add(progress uint) {
+	// TODO: Implement this method.
+}
+
+func (p *progressBar) refresh() {
+	// TODO: Implement this method.
+}
